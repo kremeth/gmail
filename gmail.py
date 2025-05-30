ada21a849e103dbf02522c9dfb88ef7fc6cca72d
kremeth <66464637+kremeth@users.noreply.github.com>
2025-05-30 14:50:05 +0800

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -17,8 +17,11 @@ EMAIL_PASSWORD = 'cqis dzyt ttqz yica'  # Use Gmail App Password
 # AUTHENTICATE WITH GOOGLE SHEETS
 # -----------------------------
 scopes = ['https://www.googleapis.com/auth/spreadsheets']
-GOOGLE_CREDENTIALS = json.loads(os.environ['GOOGLE_CREDENTIALS'])
-creds = Credentials.from_service_account_file(GOOGLE_CREDENTIALS, scopes=scopes)
+# Parse the JSON string from the Heroku config var
+creds_dict = json.loads(os.environ['GOOGLE_CREDENTIALS'])
+
+# Use from_service_account_info instead of from_service_account_file
+creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
 client = gspread.authorize(creds)
 
 sheet = client.open_by_url(SHEET_URL).sheet1  # Adjust if not first sheet
